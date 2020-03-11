5b7b188ee71db96797812c475ba67850441e1cf0
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-03-11 10:29:51 +0800

    Update mars_env.py
    1/5th waypoint - initial commit #13 @wakeuplearn

--- simulation_ws/src/rl-agent/markov/environments/mars_env.py
+++ simulation_ws/src/rl-agent/markov/environments/mars_env.py
@@ -389,8 +389,8 @@ class MarsEnv(gym.Env):
         
         
         # WayPoints to checkpoint
-        WAYPOINT_1_X = -10
-        WAYPOINT_1_Y = -4
+        WAYPOINT_1_X = self.current_distance_to_checkpoint / 5
+        WAYPOINT_1_Y = self.current_distance_to_checkpoint / 5
         
         WAYPOINT_2_X = -34 #-17
         WAYPOINT_2_Y = -3   #3
